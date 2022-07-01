3f654bcab318e226a14270cb2028ef0b5d2dba17
fernandoguzhdez <fernandoguzhdez@gmail.com>
2022-07-01 15:52:24 -0500

    Se finalizo la seccion 14 - Creando Publicaciones

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -17,7 +17,7 @@
         </div>
 
         <div class="md:w-1/2 p-10 bg-white rounded-lg shadow-xl mt-10 md:mt-0">
-            <form action="{{ route('register') }}" method="POST" novalidate>
+            <form action="{{ route('posts.store') }}" method="POST" novalidate>
                 @csrf
                 <div class="mb-5">
                     <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">
@@ -31,7 +31,7 @@
                         class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
                         value="{{ old('titulo') }}"
                     />
-                    @error('name')
+                    @error('titulo')
                         <p class="bg-red-500 text-white my-3 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
                     @enderror
                 </div>
@@ -47,9 +47,21 @@
                         class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
                     >{{ old('titulo') }}</textarea>
 
-                    @error('titulo')
+                    @error('descripcion')
                         <p class="bg-red-500 text-white my-3 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
                     @enderror
+                </div>
+
+                <div class="mb-5">
+                    <input 
+                        name="imagen"
+                        type="hidden"
+                        value="{{ old('imagen') }}"
+                    />
+                    @error('imagen')
+                        <p class="bg-red-500 text-white my-3 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+
                 </div>
 
                 <input
