4d9297c6b72788c22c4c76ebc31518f8748779ee
fernandoguzhdez <fernandoguzhdez@gmail.com>
2022-07-01 11:23:14 -0500

    probando el pull  01/07/2022

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -12,15 +12,13 @@ class ImagenController extends Controller
         $imagen = $request->file('file');
 
         $nombreImagen = Str::uuid() . "." . $imagen->extension();
-
+        
         $imagenServidor = Image::make($imagen);
 
         $imagenServidor->fit(1000, 1000);
 
         $imagenPath = public_path('uploads') . '/' . $nombreImagen;
         $imagenServidor->save($imagenPath);
-
-        echo "Nuevo";
 
         return response()->json(['imagen' => $nombreImagen]);
     }
